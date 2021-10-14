ae7314c18a7a784c3842d3894715f895ab8d91fe
Tahir321 <tariqaptech9@gmail.com>
2021-10-14 18:08:05 +0500

    update css

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -22,8 +22,35 @@ const Testimonial = () => {
                     <i class="fas fa-star"></i>
                 </div>
             </div>
+            <div className="testimonial-client-review">
+                <div className="client-img">
+                    <img src="images/client-1.png"/>
+                </div>
+                <div className="client-review">
+                    <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchangedIt has survived not only five centuries, but also the leap into electronic typesetting.</p>
+                    <h5>Rose William</h5>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+            </div>
+            <div className="testimonial-client-review">
+                <div className="client-img">
+                    <img src="images/client-1.png"/>
+                </div>
+                <div className="client-review">
+                    <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchangedIt has survived not only five centuries, but also the leap into electronic typesetting.</p>
+                    <h5>Rose William</h5>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+            </div> 
         </div>
     )
 }
-
 export default Testimonial
